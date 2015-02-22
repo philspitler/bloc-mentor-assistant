700f22be1fb46362141e6a61c9511ecf7ea7f7d5
Phil Spitler (PRS) <prs@sproutkey.com>
2015-02-22 10:15:09 -0500

    a little closer to an MVP

--- app/scripts/myscript.js
+++ app/scripts/myscript.js
@@ -1,17 +1,21 @@
-//alert('testing');
+'use strict';
+
 $(function() {
   var firstTd = 'table tr td:first-child';
 
   $(firstTd + ' i.fa-file-o').each(function (index, element) {
     var url = $(element).parent().next().find('a').attr('href');
-    var splitUrl = url.split('/');
-    var id = splitUrl.pop();
-    //var id = splitUrl[(splitUrl.length - 1)];
-    $(element).html('<button class="btn btn-primary btn-xs" data-url="'+url+'">review</button>');
+    var user = $(element).parent().next().find('a b').text();
+    $(element).html('<button class="btn btn-primary btn-xs" data-user="'+user+'" data-url="'+url+'">review</button>');
     $(element).find('button').click(function () {
+      var self = $(this);
       $.get($(this).attr('data-url'), function (data) {
-        var authenticity_token = $('meta[name=csrf-token]').attr('content');
+        //var authenticity_token = $('meta[name=csrf-token]').attr('content');
+        var title = $(data).find('.header:first');
         var contentData = $(data).find('tab_pane[title=Submission] .checkpoint-action-content');
+        $(contentData).prepend(self.attr('data-user'));
+        $(contentData).prepend(title);
+
         var button = contentData.find('.btn');
         button.attr('type','button');
         $(button).click(function () {
@@ -35,12 +39,12 @@ $(function() {
 
         $('.modal').modal('show');
         $('.modal-content').html(content);
-        $('.modal').on('hide.bs.modal', function (e) {
+        $('.modal').on('hide.bs.modal', function () {
           $('.modal-content').html('');
         });
       });
 
-    })
+    });
     //console.log($(element).parent().next().find('a').attr('href').split('/'));
     //console.log(id);
   });
