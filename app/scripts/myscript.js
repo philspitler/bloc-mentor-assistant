0d5aca6a920706aa5bd886303ad4cebba4942486
Phil Spitler (PRS) <prs@sproutkey.com>
2015-03-04 11:00:09 -0500

    fixed bug due to new assessment form
    clearing out row in dashboard after accepting
    
    added notification to top of dashboard

--- app/scripts/myscript.js
+++ app/scripts/myscript.js
@@ -10,27 +10,32 @@ $(function() {
     $(element).find('button').click(function () {
       var self = $(this);
       $.get($(this).attr('data-url'), function (data) {
-        //var authenticity_token = $('meta[name=csrf-token]').attr('content');
         var title = $(data).find('.header:first');
+        var userFullname = self.attr('data-user');
         var contentData = $(data).find('tab_pane[title=Submission] .checkpoint-action-content');
         $(contentData).prepend(title);
-        $(contentData).prepend(self.attr('data-user'));
+        $(contentData).prepend(userFullname);
         var content = $(contentData).html();
         var submitButtonAction = $(contentData).find('form').attr('action');
         var submitButtonText = $(contentData).find('form .btn').attr('value');
 
         $('.modal').modal('show');
         $('.modal-content').html(content);
-        $('.modal-content').find('form').replaceWith('<button class="btn btn-submit">'+submitButtonText+'</button>');
+        $('.modal-content').find('.btn').replaceWith('<button class="btn btn-submit">'+submitButtonText+'</button>');
+        console.log($('.modal-content').html());
         $('.modal').on('hide.bs.modal', function () {
           $('.modal-content').html('');
         });
         $('.modal-content .btn-submit').click(function () {
-          $.post(submitButtonAction, {authenticity_token: $('meta[name=csrf-token]').attr('content')}, function () {
+          $.post(submitButtonAction, $('#new_technical_assessment').serialize(), function () {
             $('.modal').modal('hide');
-            //NOT WORKING FOR SOME REASON
-            $(element).parent('tr').hide();
+            $($(element).parents('tr')[0]).hide();
+            $('.default-js-flash-container').html('<div class="alert full_page_alert success"><div class="alert-container">'+userFullname+' "'+$(title).text()+'" accepted!</div></div>');
+          }).fail(function () {
+            //USING ALERT MESSAGE UNTIL CAN MAKE A LITTLE CLEANER
+            alert('Please make sure all required fields are filled in.');
           });
+          return false;
 
         });
       });
@@ -40,7 +45,5 @@ $(function() {
     //console.log(id);
   });
 
-  $('body').append(
-    '<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"><div class="modal-dialog modal-lg"><div class="modal-content"></div></div></div>'
-  );
+  $('body').append('<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"><div class="modal-dialog modal-lg"><div class="modal-content"></div></div></div>');
 });
